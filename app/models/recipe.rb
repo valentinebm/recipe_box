a1b45e41ef2ccadbb2b2d70f00f96000fdf079fd
Valentine BM <vbmueller@bmgww.com>
2018-07-06 17:57:48 +0800

    added recipes with ingredients directions in nested form tho can not add for now

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
-  mount_uploader :image, ImageUploader
+  has_many :ingredients, inverse_of: :recipe
+  has_many :directions, inverse_of: :recipe
+
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :directions, reject_if: proc { |attributes| attributes['name'].blank? }, allow_destroy: true
+
+   mount_uploader :image, ImageUploader
+
+  validates :title, :description, :image, presence: true
 end
